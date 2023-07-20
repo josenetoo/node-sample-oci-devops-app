add06193bdcd07d78a4450a1befd699015d09e76
José Neto <53952575+josenetoo@users.noreply.github.com>
2023-07-20 16:44:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 port = 3000
 
 app.get('/', function (req, res) {
-    res.send('<h1>Bem-vindas(os)! Oracle Cloud Infrastructure DevOps Version 1!!! DevOps Experience & DevOps Heroes!!! Quinta às quinze!!!</h1>')
+    res.send('<h1>Bem-vindas(os)! Oracle Cloud Infrastructure DevOps Version 2!!! DevOps Experience & DevOps Heroes!!! Quinta às quinze!!!</h1>')
 })
 
 app.listen(port, function() {
